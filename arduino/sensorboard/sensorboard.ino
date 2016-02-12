b605b7c0eb99f79a2a7e4327ed4663306e5317e8
Adrian Holfter <adrian.holfter@student.hpi.uni-potsdam.de>
2016-02-12 14:44:16 +0100

    proper indentation, added comments

--- arduino/sensorboard/sensorboard.ino
+++ arduino/sensorboard/sensorboard.ino
@@ -5,9 +5,6 @@ constexpr int PinAdr1 = 15; // A1
 constexpr int PinAdr2 = 16; // A2
 constexpr int PinAdr3 = 17; // A3
 
-// D3, D4 data in
-// D5, D6, D7 multiplex selector D5 MSB (C) D7 LSB (A)
-
 constexpr int MultiplexInputA = 3; // D3
 constexpr int MultiplexInputB = 4; // D4
 
@@ -15,11 +12,12 @@ constexpr int MultiplexSelectA = 7; // D7 LSB
 constexpr int MultiplexSelectB = 6; // D6
 constexpr int MultiplexSelectC = 5; // D5 MSB
 
+// remapping to get consistent input pin numbering
 constexpr uint8_t pinRemapping[16] = {0, 1, 2, 3, 4, 5, 6, 7, 11, 10, 9, 8, 15, 14, 13, 12};
 
-uint32_t timestamps[16] = {0};
-uint32_t durations[16] = {0};
-uint16_t inputStates = 0; // bit field: input pin X is high
+uint32_t timestamps[16] = {0}; // last start of track signal
+uint32_t durations[16] = {0}; // last duration of track signal
+uint16_t inputStates = 0; // bit field: input pin X is currently high
 uint32_t debounceIn = 10; // time in milliseconds before input edge H/L is detected
 uint32_t debounceOut = 100; // time in milliseconds before an input edge L/H after an edge H/L is detected
 
@@ -53,55 +51,56 @@ uint8_t inputAddr = 0;
 void loop()
 {
 
-for(inputAddr = 0; inputAddr < 16; inputAddr++)
-{
-  digitalWrite(MultiplexSelectA, inputAddr & 0b001 ? HIGH : LOW);
-  digitalWrite(MultiplexSelectB, inputAddr & 0b010 ? HIGH : LOW);
-  digitalWrite(MultiplexSelectC, inputAddr & 0b100 ? HIGH : LOW);
-
-  int pinToRead = inputAddr < 8 ? MultiplexInputA : MultiplexInputB;
-  uint16_t inputAddrMask = 1 << inputAddr;
-
-  uint32_t now = millis();
-
-  if(digitalRead(pinToRead) == LOW)
+  for(inputAddr = 0; inputAddr < 16; inputAddr++)
   {
-    if((inputStates & inputAddrMask) == 0 && ((now - (timestamps[inputAddr] + durations[inputAddr])) > debounceOut))
+    digitalWrite(MultiplexSelectA, inputAddr & 0b001 ? HIGH : LOW);
+    digitalWrite(MultiplexSelectB, inputAddr & 0b010 ? HIGH : LOW);
+    digitalWrite(MultiplexSelectC, inputAddr & 0b100 ? HIGH : LOW);
+
+    // the multiplexers are really quick -
+    // the Arduino is slow enough that we don't have to delay reading the data
+  
+    int pinToRead = inputAddr < 8 ? MultiplexInputA : MultiplexInputB;
+    uint16_t inputAddrMask = 1 << inputAddr;
+  
+    uint32_t now = millis();
+  
+    if(digitalRead(pinToRead) == LOW) // inverting input logic
     {
-      Serial.print(pinRemapping[inputAddr]);
-      Serial.print(" ");
-      Serial.print(now);
-      Serial.print(" ");
-      Serial.println(0);
-            
-      timestamps[inputAddr] = now;
-      inputStates |= inputAddrMask;
-
-      CAN::send(inputAddr, timestamps[inputAddr], 0);
+      if((inputStates & inputAddrMask) == 0 && ((now - (timestamps[inputAddr] + durations[inputAddr])) > debounceOut))
+      {
+        Serial.print(pinRemapping[inputAddr]);
+        Serial.print(" ");
+        Serial.print(now);
+        Serial.print(" ");
+        Serial.println(0);
+              
+        timestamps[inputAddr] = now;
+        inputStates |= inputAddrMask;
+  
+        CAN::send(inputAddr, timestamps[inputAddr], 0);
+      }
     }
-  }
-  else
-  {
-    if((inputStates & inputAddrMask) > 0 && ((now - timestamps[inputAddr]) > debounceIn))
+    else
     {
-      Serial.print(pinRemapping[inputAddr]);
-      Serial.print(" ");
-      durations[inputAddr] = now - timestamps[inputAddr];
-      Serial.print(timestamps[inputAddr]);
-      Serial.print(" ");
-      Serial.println(durations[inputAddr]);
-      inputStates &= ~inputAddrMask;  
-
-      // Send message with timestamp and duration
-      CAN::send(inputAddr, timestamps[inputAddr], durations[inputAddr]);
+      if((inputStates & inputAddrMask) > 0 && ((now - timestamps[inputAddr]) > debounceIn))
+      {
+        Serial.print(pinRemapping[inputAddr]);
+        Serial.print(" ");
+        durations[inputAddr] = now - timestamps[inputAddr];
+        Serial.print(timestamps[inputAddr]);
+        Serial.print(" ");
+        Serial.println(durations[inputAddr]);
+        
+        inputStates &= ~inputAddrMask;  
+  
+        // Send message with timestamp and duration
+        CAN::send(inputAddr, timestamps[inputAddr], durations[inputAddr]);
+      }
     }
+  
+    CAN::processEvents();
   }
-
-  CAN::processEvents();
-
-}
-
-
 }
 
 void msgHandler(CAN::CANAddress address, uint32_t timestamp, uint32_t duration)
